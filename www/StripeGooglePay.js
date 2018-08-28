1acdd6cee7fd6cdb2170b4d7abea3f46c5178855
ryssa <lari.barabasz@gmail.com>
2018-08-28 11:01:55 -0300

    Initial commit

---
+++ www/StripeGooglePay.js
@@ -0,0 +1,30 @@
+﻿function StripeGooglePay() {
+}
+
+StripeGooglePay.prototype.isAvailable = function () {
+    return new Promise(function (resolve, reject) {
+        cordova.exec(resolve, reject, 'StripeGooglePay', 'isAvailable', []);
+    });
+};
+
+StripeGooglePay.prototype.setKey = function (key) {
+    return new Promise(function (resolve, reject) {
+        cordova.exec(resolve, reject, 'StripeGooglePay', 'setKey', [key]);
+    });
+};
+
+StripeGooglePay.prototype.pay = function (amount, currency) {
+    return new Promise(function (resolve, reject) {
+        cordova.exec(resolve, reject, 'StripeGooglePay', 'pay', [amount, currency]);
+    });
+};
+
+StripeGooglePay.install = function () {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+    window.plugins.stripeGooglePay = new StripeGooglePay();
+    return window.plugins.stripeGooglePay;
+};
+
+cordova.addConstructor(StripeGooglePay.install);
